2472e2fbfe5352545b09b1428b6c4f226ffd071d
Doroteo111 <ladoritaesta@gmail.com>
2023-05-15 10:36:29 +0200

    queeeeeeeee

--- Assets/SCRPIT/TesteoAnimacionENE.cs
+++ Assets/SCRPIT/TesteoAnimacionENE.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class TesteoAnimacionENE : MonoBehaviour
 {
     private Animator _animator;
-
+    private int hearts = 2;
+    public bool isGameOver;
     void Start()
     {
         _animator = GetComponentInChildren<Animator>();
@@ -14,15 +15,43 @@ public class TesteoAnimacionENE : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) //left botton down
+        if (Input.GetKey("w"))
+        {
+            _animator.SetBool("isWalking", true);
+        }
+        if (!Input.GetKey("w"))
+        {
+            _animator.SetBool("isWalking",false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
             Attack();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            hearts--;
+            Debug.Log("hearts");
         }
     }
 
     private void Attack()
     {
-        _animator.SetInteger("Attack_type", Random.Range(1, 3));
-        _animator.SetTrigger("Attack");
+        _animator.SetTrigger("Attack_1");
+    }
+
+    public void MinusLife()
+    {
+      
+        if (hearts <= 0)
+        {
+
+            GameOver();
+        }
+    }
+    public void GameOver()
+    {
+        isGameOver = true;
+        _animator.SetBool("isGameOver", true);
     }
 }
